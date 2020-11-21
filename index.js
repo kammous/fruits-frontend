66c4c5366e284357a08bb14ce3f660785f8e4878
kammous <kimo_ammous@yahoo.fr>
2020-11-21 15:46:49 +0100

    import project

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var proxy = require('express-http-proxy');
+var app = express()
+
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/dist'))
+
+var backend_service = process.env.BACKEND_SERVICE || undefined
+if (backend_service == undefined) {
+  console.error("CONFIG ERROR: Can't find backend service config! \nDefine `BACKEND_SERVICE` env variable.")
+} else {
+  console.info("Backend service defined at `" + backend_service + "`. Proxying all request to /api/*")
+  app.use('/api', proxy(backend_service, {
+    proxyReqPathResolver: function (req) {
+      return '/api' + req.url
+    }})
+  );
+}
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
---
+++ shortcuts/complete-part-3.sh
@@ -0,0 +1,21 @@
+##!/bin/bash
+
+################################################################################
+# Complete the part III of the tutorial  #
+################################################################################
+
+# Start creating a production project.
+oc new-project fruits-catalog-prod
+oc adm policy add-role-to-user edit system:serviceaccount:fruits-catalog-dev:jenkins -n fruits-catalog-prod
+oc adm policy add-role-to-group system:image-puller system:serviceaccounts:fruits-catalog-prod -n fruits-catalog-dev
+
+# Deploy a Jenkins instance and pipelines for components.
+cd ..
+oc new-app jenkins-persistent -l app=jenkins -p MEMORY_LIMIT=1Gi -n fruits-catalog-dev
+oc create -f pipelines/fruits-cicd-pipeline.yml -n fruits-catalog-dev
+
+# Deploy all the components template and apply it manifests.
+oc create -f manifests/template-prod.yml
+oc process fruits-catalog-prod -n fruits-catalog-prod | oc create -f - -n fruits-catalog-prod
+
+oc start-build fruits-cicd-pipeline -n fruits-catalog-dev
